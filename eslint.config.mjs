fe4a44d1ceaf41211f88ffd23c9b8a3d55c8487b
drago1520 <141066422+drago1520@users.noreply.github.com>
2025-11-03 06:38:52 +0200

    fix: db:types & db:importmap

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import importPlugin from "eslint-plugin-import";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  {
-    plugins: {
-      import: importPlugin,
-    },
-    settings: { //Added because it can't compile without extentions for local files
-      "import/extensions": [
-        ".js",
-        ".jsx",
-        ".ts",
-        ".tsx",
-        ".mjs",
-        ".cjs",
-        ".mts",
-        ".cts",
-        ".d.ts",
-        ".css",
-        ".scss",
-        ".sass",
-        ".less",
-        ".pcss",
-        ".postcss",
-        ".tailwind",
-        ".svg",
-      ],
-    },
-    rules: {
-      "import/extensions": ["error", "always", { ignorePackages: true }],
-    },
-  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
